a584c3b9a3f391d326986d565009d4ffec34ae74
Jascha Base <basejascha@gmail.com>
2025-09-29 14:16:04 +0800

    feature(ShelfSmart): create dashboard page

---
+++ ShelfSmart/dashboard/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def dashboard_view(request):
+    # Simulate getting user role from DB (change this to "Admin" to test admin view)
+    role = "User"
+
+    context = {
+        "user_name": "Nisal Gunasekara",
+        "role": role,
+        "total_users": "150",
+        "total_books": "1500",
+        "branch_count": "10",
+        "overdue_borrowers": ["Sasmith Gunasekara (ID: 10)", "Dulani Perera (ID: 12)"],
+        "admins": ["Nisal Gunasekara (Active)", "Sithija Perera (Inactive)"],
+        "branches": [
+            "BookWorm - Matara (ID:1)",
+            "BookWorm - Colombo (ID:2)"
+        ],
+    }
+    return render(request, "dashboard/dashboard.html", context)
